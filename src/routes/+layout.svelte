78218d33a002d6565d29ece1759965ef6abcb7e2
Sachsenspielt <sachsenspielt3@gmail.com>
2024-05-05 15:32:27 +0200

    added some legal stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,16 @@
 <script>
-import { DarkMode } from 'flowbite-svelte';
+	import { Button, DarkMode } from 'flowbite-svelte';
 </script>
 
 <div class="absolute top-4 right-4">
 	<DarkMode />
 </div>
 
-<main class="bg-background-light dark:bg-background min-h-screen text-text-light dark:text-text">
+<main class="bg-background-light dark:bg-background min-h-screen overflow-y-auto text-text-light dark:text-text">
 	<slot />
 </main>
+
+<div class="absolute bottom-4 right-4">
+	<Button href="/imprint" color="alternative" size="xs">Impressum</Button>
+	<Button href="/privacy" color="alternative" size="xs">Datenschutz</Button>
+</div>
---
+++ src/routes/imprint/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { ArrowLeftOutline, BackwardStepSolid } from 'flowbite-svelte-icons';
+	import { Button } from 'flowbite-svelte';
+</script>
+
+<section class="w-full md:max-w-[50%] h-screen p-16">
+	<Button href="/" color="alternative"><ArrowLeftOutline/></Button>
+	<h1 class="text-4xl my-8">Vertretungsapp<span class="text-primary">.</span></h1>
+
+	<h1>Impressum</h1>
+	<p>Finn Margraf<br />
+		Kocorstra&szlig;e 5<br />
+		02977 Hoyerswerda</p>
+	<h2 class="mt-4">Kontakt</h2>
+	<p>Telefon: 03571 6088641<br />
+		E-Mail: support@vertretungsapp.de</p>
+	<h2 class="mt-4">Haftung f&uuml;r Inhalte</h2>
+	<p>Als Diensteanbieter sind wir gem&auml;&szlig; &sect; 7 Abs.1 TMG f&uuml;r eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach &sect;&sect; 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, &uuml;bermittelte oder gespeicherte fremde Informationen zu &uuml;berwachen oder nach Umst&auml;nden zu forschen, die auf eine rechtswidrige T&auml;tigkeit hinweisen.</p>
+	<h2 class="mt-4">Haftung f&uuml;r Links</h2>
+	<p>Unser Angebot enth&auml;lt Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb k&ouml;nnen wir f&uuml;r diese fremden Inhalte auch keine Gew&auml;hr &uuml;bernehmen. F&uuml;r die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf m&ouml;gliche Rechtsverst&ouml;&szlig;e &uuml;berpr&uuml;ft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.</p>
+	<h2 class="mt-4">G&uuml;ltigkeit</h2>
+	<ul class="list-disc list-inside">
+		<li>
+			<a href="https://vertretungsapp.de" class="text-primary">https://vertretungsapp.de</a>
+		</li>
+		<li>
+			<a href="https://www.vertretungsapp.de" class="text-primary">https://www.vertretungsapp.de</a>
+		</li>
+		<li>
+			<a href="https://app.vertretungsapp.de" class="text-primary">https://app.vertretungsapp.de</a>
+		</li>
+	</ul>
+
+</section>
